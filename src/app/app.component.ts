b00dbdf7c30f1210bc84bea05de4f188552e3457
michael-graute <m.graute@team-braeutigam.de>
2024-02-10 10:08:03 +0100

    work in progress

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,35 @@ import {Component} from '@angular/core';
 })
 export class AppComponent {
 
+  _keyboardVisible: boolean = true;
+  _sequencerVisible: boolean = true;
+  keyboardHeight: string = '300px';
+  sequencerHeight: string = '550px';
+
+  gridTemplateRows: string[] = ['80px', '1fr', this.sequencerHeight, this.keyboardHeight];
+
   public page: string = 'main';
 
+  set keyboardVisible(value: boolean) {
+    this._keyboardVisible = value;
+    this.gridTemplateRows[3] = this._keyboardVisible ? this.keyboardHeight : '30px';
+    this.gridTemplateRows = JSON.parse(JSON.stringify(this.gridTemplateRows));
+  }
+
+  getGridTemplateRows(): string {
+    return this.gridTemplateRows.join(' ');
+  }
+
+  get keyboardVisible(): boolean {
+    return this._keyboardVisible;
+  }
+
+  set sequencerVisible(value: boolean) {
+    this._sequencerVisible = value;
+    this.gridTemplateRows[2] = this._sequencerVisible ? this.sequencerHeight : '30px';
+  }
+
+  get sequencerVisible(): boolean {
+    return this._sequencerVisible;
+  }
 }
