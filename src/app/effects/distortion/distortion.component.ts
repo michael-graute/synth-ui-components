d64fbaf64f408adfe9573ebe20c4a2b9f732e9db
michael-graute <m.graute@team-braeutigam.de>
2024-02-26 14:46:25 +0100

    work in progress

---
+++ src/app/effects/distortion/distortion.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ins-distortion',
+  standalone: true,
+  imports: [],
+  templateUrl: './distortion.component.html',
+  styleUrl: './distortion.component.scss'
+})
+export class DistortionComponent {
+
+}
