175ba148ddd9f4c52f3586b4893e62240e374fbf
michael-graute <m.graute@team-braeutigam.de>
2024-03-04 19:47:33 +0100

    work in progress

--- src/app/instruments/sampler/sampler.component.ts
+++ src/app/instruments/sampler/sampler.component.ts
@@ -18,6 +18,54 @@ export type SamplerConfig = {
   styleUrl: './sampler.component.scss'
 })
 export class SamplerComponent extends AbstractSynthComponent<SamplerConfig>{
+
+
+  samplePacks= {
+    'flute-clean': {
+      urls: {
+        A2: "FluteClean_A2.wav",
+        C2: "FluteClean_C2.wav",
+        D2: "FluteClean_D2.wav",
+      },
+      baseUrl: '/assets/audio/sample-packs/flute-clean/'
+    },
+    'salamander': {
+      urls: {
+        A0: "A0.mp3",
+        C1: "C1.mp3",
+        "D#1": "Ds1.mp3",
+        "F#1": "Fs1.mp3",
+        A1: "A1.mp3",
+        C2: "C2.mp3",
+        "D#2": "Ds2.mp3",
+        "F#2": "Fs2.mp3",
+        A2: "A2.mp3",
+        C3: "C3.mp3",
+        "D#3": "Ds3.mp3",
+        "F#3": "Fs3.mp3",
+        A3: "A3.mp3",
+        C4: "C4.mp3",
+        "D#4": "Ds4.mp3",
+        "F#4": "Fs4.mp3",
+        A4: "A4.mp3",
+        C5: "C5.mp3",
+        "D#5": "Ds5.mp3",
+        "F#5": "Fs5.mp3",
+        A5: "A5.mp3",
+        C6: "C6.mp3",
+        "D#6": "Ds6.mp3",
+        "F#6": "Fs6.mp3",
+        A6: "A6.mp3",
+        C7: "C7.mp3",
+        "D#7": "Ds7.mp3",
+        "F#7": "Fs7.mp3",
+        A7: "A7.mp3",
+        C8: "C8.mp3"
+      },
+      baseUrl: 'https://tonejs.github.io/audio/salamander/'
+    }
+  }
+
   @Input() name: string = 'Sampler';
   public override config: SamplerConfig = {
     active: true,
@@ -25,39 +73,8 @@ export class SamplerComponent extends AbstractSynthComponent<SamplerConfig>{
     release: 1,
     attack: 0,
     curve: 'linear',
-    urls: {
-      A0: "A0.mp3",
-      C1: "C1.mp3",
-      "D#1": "Ds1.mp3",
-      "F#1": "Fs1.mp3",
-      A1: "A1.mp3",
-      C2: "C2.mp3",
-      "D#2": "Ds2.mp3",
-      "F#2": "Fs2.mp3",
-      A2: "A2.mp3",
-      C3: "C3.mp3",
-      "D#3": "Ds3.mp3",
-      "F#3": "Fs3.mp3",
-      A3: "A3.mp3",
-      C4: "C4.mp3",
-      "D#4": "Ds4.mp3",
-      "F#4": "Fs4.mp3",
-      A4: "A4.mp3",
-      C5: "C5.mp3",
-      "D#5": "Ds5.mp3",
-      "F#5": "Fs5.mp3",
-      A5: "A5.mp3",
-      C6: "C6.mp3",
-      "D#6": "Ds6.mp3",
-      "F#6": "Fs6.mp3",
-      A6: "A6.mp3",
-      C7: "C7.mp3",
-      "D#7": "Ds7.mp3",
-      "F#7": "Fs7.mp3",
-      A7: "A7.mp3",
-      C8: "C8.mp3"
-    },
-    baseUrl: 'https://tonejs.github.io/audio/salamander/'
+    urls: this.samplePacks.salamander.urls,
+    baseUrl: this.samplePacks.salamander.baseUrl
   };
   protected override componentType: string = 'instrument';
   protected override instrument: Tone.Sampler = new Tone.Sampler(this.config.urls, this.loaded.bind(this), this.config.baseUrl);
@@ -116,5 +133,10 @@ export class SamplerComponent extends AbstractSynthComponent<SamplerConfig>{
 
   get baseUrl(): string {
     return this.config.baseUrl;
+  }
+
+  loadSamplePack(name: string) {
+    // @ts-ignore
+    this.instrument = new Tone.Sampler(this.samplePacks[name].urls, this.loaded.bind(this), this.samplePacks[name].baseUrl);
   }
 }
