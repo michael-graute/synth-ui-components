e42240d7cae93d9707c2b36ce0796d6e4ac77b9f
michael-graute <m.graute@team-braeutigam.de>
2024-03-17 17:12:08 +0100

    work in progress

--- src/app/piano-roll/piano-roll.component.ts
+++ src/app/piano-roll/piano-roll.component.ts
@@ -1,9 +1,11 @@
-import {ChangeDetectorRef, Component} from '@angular/core';
+import {ChangeDetectorRef, Component, Input, OnInit} from '@angular/core';
 import * as Tone from "tone";
 import {SynthService} from "../synth.service";
 import {Subject} from "rxjs";
+import {v4 as uuidv4} from "uuid";
+import {InsPreset, PresetManagerService} from "../preset-manager/preset-manager.service";
 
-export interface PianoRollStep {
+export type PianoRollStep = {
   id: string;
   velocity: number;
   duration: string;
@@ -13,28 +15,98 @@ export interface PianoRollStep {
   note: string;
 }
 
+export type PianoRollConfig = {
+  availableSteps: {[key: string]: PianoRollStep[]};
+  stepCount: number;
+  activeStepCount: number;
+  interval: number;
+  tempo: number;
+}
+
 @Component({
   selector: 'ins-piano-roll',
   templateUrl: './piano-roll.component.html',
   styleUrl: './piano-roll.component.scss'
 })
-export class PianoRollComponent {
+export class PianoRollComponent implements OnInit {
 
-  public tempo: number = 120;
-  public interval: any = "4n";
+  private config: PianoRollConfig = {
+    availableSteps: {},
+    stepCount: 16,
+    activeStepCount: 8,
+    interval: 2,
+    tempo: 120,
+  }
+
+  @Input() id: string = uuidv4();
+
   public playing: boolean = false;
   public paused: boolean = false;
   public currentStep: number = 0;
   public loop: Tone.Loop | undefined;
   public notes : string[] = ["C","C#","D","D#","E","F","F#","G","G#","A","A#","B"];
   public octaves : number[] = [1,2,3];
-  public availableSteps : {[key: string]: PianoRollStep[]} = {};
-  public stepCount: number = 16;
+  public gateOptions: string[] = ['1n', '2n', '4n', '8n', '16n', '32n', '64n'];
 
   stepPlaying: Subject<number> = new Subject<number>();
 
-  constructor(public synthService: SynthService, public changeDetectorRef: ChangeDetectorRef) {
+  set availableSteps(value: {[key: string]: PianoRollStep[]}) {
+    this.config.availableSteps = value;
+  }
+
+  get availableSteps(): {[key: string]: PianoRollStep[]} {
+    return this.config.availableSteps;
+  }
+
+  set stepCount(value: number) {
+    this.config.stepCount = value;
+  }
+
+  get stepCount(): number {
+    return this.config.stepCount;
+  }
+
+  set activeStepCount(value: number) {
+    this.config.activeStepCount = value;
+  }
+
+  get activeStepCount(): number {
+    return this.config.activeStepCount;
+  }
+
+  set tempo(value: number) {
+    this.config.tempo = value;
+  }
+
+  get tempo(): number {
+    return this.config.tempo;
+  }
+
+  set interval(value: number) {
+    this.config.interval = value;
+  }
+  get interval(): number {
+    return this.config.interval;
+  }
+
+  constructor(public synthService: SynthService, public changeDetectorRef: ChangeDetectorRef, private presetManagerService: PresetManagerService) {
     this.buildSteps();
+  }
+
+  ngOnInit(): void {
+    this.presetManagerService.loadConfigEvent.subscribe((preset: InsPreset): void => {
+      if(preset.components[this.id]) {
+        this.config = preset.components[this.id];
+      }
+    });
+    this.presetManagerService.saveConfigEvent.subscribe((presetId: string): void => {
+      const preset: string | null = localStorage.getItem(presetId);
+      if(preset) {
+        let presetConfig = JSON.parse(preset);
+        presetConfig.components[this.id] = this.config;
+        localStorage.setItem(presetId, JSON.stringify(presetConfig));
+      }
+    });
   }
 
   buildSteps(): void {
@@ -66,7 +138,6 @@ export class PianoRollComponent {
     this.paused = false;
     let index = this.currentStep;
     this.loop = new Tone.Loop((time: number): void => {
-      console.log(index);
       for (let availableStepsKey in this.availableSteps) {
         if (this.availableSteps.hasOwnProperty(availableStepsKey)) {
           const step: PianoRollStep = this.availableSteps[availableStepsKey][index];
@@ -78,12 +149,11 @@ export class PianoRollComponent {
       Tone.Draw.schedule((): void => {
         this.currentStep = index;
         this.stepPlaying.next(index);
-        console.log(this.currentStep);
-        index = (index + 1) % this.stepCount;
+        index = (index + 1) % this.activeStepCount;
         this.changeDetectorRef.detectChanges();
       }, time);
 
-    }, this.interval).start(0);
+    }, this.gateOptions[this.interval]).start(0);
     Tone.Transport.bpm.value = this.tempo;
     Tone.Transport.start();
   }
@@ -94,13 +164,15 @@ export class PianoRollComponent {
     this.paused = false;
     this.loop?.stop();
     Tone.Transport.stop();
+    this.changeDetectorRef.detectChanges();
   }
 
   pause(): void {
     this.playing = false;
     this.paused = true;
     this.loop?.stop();
-    Tone.Transport.pause();
+    Tone.Transport.stop();
+    this.changeDetectorRef.detectChanges();
   }
 
 }
