bc01e200290de027b710d7468f63ba3d59aee175
michael-graute <m.graute@team-braeutigam.de>
2024-04-21 13:47:03 +0200

    work in progress

--- src/app/ui-elements/transport-control/transport-control.component.ts
+++ src/app/ui-elements/transport-control/transport-control.component.ts
@@ -7,19 +7,30 @@ import {Component, EventEmitter, Input, Output} from '@angular/core';
 })
 export class TransportControlComponent {
 
-  public playing: boolean = false;
-  public recording: boolean = false;
-  public paused: boolean = false;
+  @Input() label: string | undefined;
+
+  @Input() disabled: boolean = false;
+  @Input() playing: boolean = false;
+  @Input() recording: boolean = false;
+  @Input() paused: boolean = false;
 
   @Input() playButton: boolean = true;
   @Input() pauseButton: boolean = true;
   @Input() stopButton: boolean = true;
   @Input() recordButton: boolean = true;
+  @Input() fastButtons: boolean = false;
+  @Input() skipButtons: boolean = false;
 
   @Output() playButtonClick: EventEmitter<void> = new EventEmitter<void>();
   @Output() stopButtonClick: EventEmitter<void> = new EventEmitter<void>();
   @Output() pauseButtonClick: EventEmitter<void> = new EventEmitter<void>();
   @Output() recordButtonClick: EventEmitter<void> = new EventEmitter<void>();
+  @Output() skipForwardButtonClick: EventEmitter<void> = new EventEmitter<void>();
+  @Output() skipBackwardButtonClick: EventEmitter<void> = new EventEmitter<void>();
+  @Output() fastForwardButtonDown: EventEmitter<void> = new EventEmitter<void>();
+  @Output() fastForwardButtonUp: EventEmitter<void> = new EventEmitter<void>();
+  @Output() fastBackwardButtonDown: EventEmitter<void> = new EventEmitter<void>();
+  @Output() fastBackwardButtonUp: EventEmitter<void> = new EventEmitter<void>();
 
 
   onPlayButtonClick(): void {
@@ -41,5 +52,29 @@ export class TransportControlComponent {
   onRecordButtonClick(): void {
     this.recording = !this.recording;
     this.recordButtonClick.emit();
+  }
+
+  onSkipBackButtonClick(): void {
+    this.skipBackwardButtonClick.emit();
+  }
+
+  onSkipForwardButtonClick(): void {
+    this.skipForwardButtonClick.emit();
+  }
+
+  onFFWDButtonDown(): void {
+    this.fastForwardButtonDown.emit();
+  }
+
+  onFFWDButtonUp(): void {
+    this.fastForwardButtonUp.emit();
+  }
+
+  onFBWDButtonDown(): void {
+    this.fastBackwardButtonDown.emit();
+  }
+
+  onFBWDButtonUp(): void {
+    this.fastBackwardButtonUp.emit();
   }
 }
